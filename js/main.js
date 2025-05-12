20faebbb7d7e8555a3e64711dc298993df80828d
Vera Kippel <veki2400@student.miun.se>
2025-05-12 20:55:22 +0200

    gör ny html-sida för registrering

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const productList = document.getElementById("product-list");
+
 const menu = document.getElementById("menu");
 
 //formulär
@@ -8,8 +8,23 @@ const productForm = document.getElementById("product-form");
 window.onload = init;
 
 function init() {
-    if(productList) {
-        getProducts();
+
+    changeMenu();
+}
+
+function changeMenu(){
+    if(localStorage.getItem("webshop_token")){
+        menu.innerHTML = `
+        <li><a href="index.html">Startsida</a></li>
+        <li><a href="admin.html">Admin</a></li>
+        <li><button id="logout-button" class="logout-button">Logga ut</button></li>
+        `
+    }else{
+        menu.innerHTML = `
+        <li><a href="index.html">Startsida</a></li>
+        <li><a href="register.html">Registering</a></li>
+        <li><a href="login.html" id="login-button" class="auth-button">Logga in</a></li>
+        `
     }
 }
 
