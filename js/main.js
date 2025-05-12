ba60aefe76d483439c5c01a75a2ef84d05bd93f0
Vera Kippel <veki2400@student.miun.se>
2025-05-12 22:25:35 +0200

    fixar knapp till en länk, fixar så att logga ut fungerar och registrering med nytt fetch

--- js/main.js
+++ js/main.js
@@ -1,25 +1,32 @@
 
 const menu = document.getElementById("menu");
-const logoutBtn = document.getElementById("logout-button");
+
 
 //formulär
 const loginForm = document.getElementById("login-form");
-const productForm = document.getElementById("register-form");
+const registerForm = document.getElementById("register-form");
 
-if(logoutBtn){
-    logoutBtn.addEventListener("click", function() {
+window.onload = init;
+
+function updateMenu() {
+    const logoutBtn = document.getElementById("logout-button");
+    if(logoutBtn){
+    logoutBtn.addEventListener("click", () => {
+        console.log("klick på loggga ut")
         localStorage.removeItem("webshop_token");
         window.location.href = "login.html";
     });
 }
-
-window.onload = init;
+}
 
 function init() {
     changeMenu();
 
     if(loginForm){
         loginForm.addEventListener("submit", loginUser);
+    }
+    if(registerForm){
+        registerForm.addEventListener("submit", registerUser);
     }
 }
 
@@ -28,8 +35,9 @@ function changeMenu(){
         menu.innerHTML = `
         <li><a href="index.html">Startsida</a></li>
         <li><a href="admin.html">Admin</a></li>
-        <li><button id="logout-button" class="logout-button">Logga ut</button></li>
+        <li><a href="" id="logout-button" class="logout-button">Logga ut</a></li>
         `
+        updateMenu();
     }else{
         menu.innerHTML = `
         <li><a href="index.html">Startsida</a></li>
@@ -74,4 +82,33 @@ async function loginUser(e) {
     } catch (error) {
         console.log("Felaktigt användarnamn eller lösenord!");
     }
+}
+
+async function registerUser(e){
+    e.preventDefault();
+
+    let usernameInput = document.getElementById("username").value;
+    let passwordInput = document.getElementById("password").value;
+
+    if(!usernameInput || !passwordInput) {
+        console.log("Fyll i alla fält!");
+        return;
+    }
+
+    let user = {
+        username: usernameInput,
+        password: passwordInput
+    }
+    fetch("http://localhost:3001/api/register", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(user)
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data);
+        window.location.href = "admin.html";
+    })
 }
