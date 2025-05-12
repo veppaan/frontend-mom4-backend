1a253bc293bd580cba6505b9ef9a1139ee8371e0
Vera Kippel <veki2400@student.miun.se>
2025-05-12 21:33:20 +0200

    skriver ut token och login-objekt och fetch-anrop till del1

--- js/main.js
+++ js/main.js
@@ -3,13 +3,16 @@ const menu = document.getElementById("menu");
 
 //formulär
 const loginForm = document.getElementById("login-form");
-const productForm = document.getElementById("product-form");
+const productForm = document.getElementById("register-form");
 
 window.onload = init;
 
 function init() {
-
     changeMenu();
+
+    if(loginForm){
+        loginForm.addEventListener("submit", loginUser);
+    }
 }
 
 function changeMenu(){
@@ -28,4 +31,39 @@ function changeMenu(){
     }
 }
 
-//Hämta produkter
+async function loginUser(e) {
+    e.preventDefault();
+
+    let usernameInput = document.getElementById("username").value;
+    let passwordInput = document.getElementById("password").value;
+
+    if(!usernameInput || !passwordInput) {
+        console.log("Fyll i alla fält!");
+        return;
+    }
+
+    let user = {
+        username: usernameInput,
+        password: passwordInput
+    }
+    try {
+        const resp = await fetch("http://localhost:3001/api/login", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+
+        if(resp.ok) {
+            const data = await resp.json();
+            
+            localStorage.setItem("webshop_token", data.token);
+            window.location.href = "admin.html";
+        }else{
+            throw error;
+        }
+    } catch (error) {
+        console.log("Felaktigt användarnamn eller lösenord!");
+    }
+}
