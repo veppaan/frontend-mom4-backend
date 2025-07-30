c33f82d8f43a660036c01307c59850694d591bc4
Vera Kippel <veki2400@student.miun.se>
2025-07-30 13:59:51 +0200

    kollar giltighet på token

--- js/checkAuth.js
+++ js/checkAuth.js
@@ -2,4 +2,24 @@
 
 if(!localStorage.getItem("token")){
     window.location.href = "login.html";
+} else {
+    const token = localStorage.getItem("token");
+    try {
+        const resp = await fetch("https://backend-mom4-1.onrender.com/api/token", {
+            method: "GET",
+            headers: {
+                "content-type": "application/json",
+                "Authorization": `Bearer ` + token
+            }
+        })
+
+        if(resp.ok) {
+            const data = await resp.json();
+            console.log(data);
+        }else{
+            window.location.href = "login.html";
+        }
+    } catch (error) {
+        console.log("Det blev något fel med token-autentisering!");
+    }
 }
